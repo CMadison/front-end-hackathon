0c780a477e6f6c516ea383710143ea1f4315ea02
Chris Madison <CKMadison4@gmail.com>
2016-07-10 16:56:48 -0700

    Added third party library, and front-end landing page. Next steps are detailed in the readme file.

---
+++ js/application.js
@@ -0,0 +1,12 @@
+// var traitify = require('traitify');
+
+//   traitify.setHost("https://api-sandbox.traitify.com");
+//   traitify.setVersion("v1");
+//   traitify.setSecretKey("r6redimhllt4snco61rr5g9mk5");
+
+//       Traitify.setPublicKey("519okkqomh8n81luoda2felchp");
+//       Traitify.setHost("https://api-sandbox.traitify.com");
+//       Traitify.setVersion("v1");
+//       var assessmentId = '9b1a9e7e-8d5e-401e-a339-824e627f7126';
+//       Traitify.ui.load(assessmentId, ".assessment")
+
